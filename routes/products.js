b0e065bdb0dd580b19f07e912616b8128a960cf8
claurennt <c.benedetti1989@gmail.com>
2021-09-10 19:19:05 +0200

    use path-to-regexp module, refactor models export and usage in controllers

--- routes/products.js
+++ routes/products.js
@@ -4,19 +4,27 @@ const productsRouter = express.Router();
 //controllers imports
 const insert_new_product = require('../controllers/post_controllers');
 const {
-  get_all_products_by_category,
+  get_all_products_of_collection_by_gender,
   get_one_product_by_id,
 } = require('../controllers/get_controllers');
 const delete_product_by_id = require('../controllers/delete_controllers');
 
 //routes
 productsRouter
-  .route('/:genere/:categoria')
+  .route(
+    //use path-to-regexp module to limit the params
+    '/:genere(donna|uomo|kids)/:categoria(Maglie|Intimo|Canottiere|Vestaglie|Pigiami|Calze)'
+  )
   .post(insert_new_product)
-  .get(get_all_products_by_category);
+  .get(get_all_products_of_collection_by_gender);
 
-productsRouter.route('/:genere/:categoria/:id').get(get_one_product_by_id);
+productsRouter
+  .route(
+    //use path-to-regexp module to limit the params
+    '/:genere(donna|uomo|kids)/:categoria(Maglie|Intimo|Canottiere|Vestaglie|Pigiami|Calze)/:id'
+  )
+  .get(get_one_product_by_id);
 
-productsRouter.route('/:categoria/:id').delete(delete_product_by_id);
+productsRouter.route('/:id').delete(delete_product_by_id);
 
 module.exports = productsRouter;
