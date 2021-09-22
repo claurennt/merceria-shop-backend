cd4cb24a38b1ad1e6fb3f156c141c19c540babc3
claurennt <c.benedetti1989@gmail.com>
2021-09-22 18:56:24 +0200

    add post form, need fix of image serving

--- controllers/post_controllers.js
+++ controllers/post_controllers.js
@@ -3,38 +3,36 @@ const defineCollection = require('../utils/defineCollection');
 
 //controller to insert a new document in the collection
 const insert_new_product = async (req, res, next) => {
-  // const {
-  //   pic_src,
-  //   name,
-  //   category,
-  //   gender,
-  //   brand,
-  //   material,
-  //   color,
-  //   size,
-  //   price,
-  //   description,
-  // } = req.body;
-  console.log({ file: req.file });
-  console.log(req.body);
-  // if (
-  //   !pic_src ||
-  //   !name ||
-  //   !category ||
-  //   !gender ||
-  //   !brand ||
-  //   !material ||
-  //   !color ||
-  //   !size ||
-  //   !price ||
-  //   !description
-  // )
-  //   //if the req.body has missing values notify the user and exit
-  //   return res
-  //     .status(404)
-  //     .send(
-  //       'Bad Request: please provide values for the all the following keys: pic_src, name, category, gender, brand, material, color, size, price, description.'
-  //     );
+  const {
+    name,
+    category,
+    gender,
+    brand,
+    material,
+    color,
+    size,
+    price,
+    description,
+  } = req.body;
+  const { path } = req.file;
+  console.log({ body: req.body, file: req.file });
+  if (
+    !name ||
+    !category ||
+    !gender ||
+    !brand ||
+    !material ||
+    !color ||
+    !size ||
+    !price ||
+    !description
+  )
+    //if the req.body has missing values notify the user and exit
+    return res
+      .status(404)
+      .send(
+        'Bad Request: please provide values for the all the following keys: pic_src, name, category, gender, brand, material, color, size, price, description.'
+      );
 
   try {
     //define the collection we are using based on the url param
@@ -42,8 +40,18 @@ const insert_new_product = async (req, res, next) => {
 
     //create the new product document in the collection with the req.body data
     const newProduct = await Collection.create({
-      ...req.body,
+      name,
+      category,
+      gender,
+      brand,
+      material,
+      color,
+      size,
+      price,
+      description,
+      pic_src: path,
     });
+    console.log({ new: newProduct });
     //send back the new added document
     res.json(newProduct);
   } catch (err) {
---
+++ utils/multerStorage.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '/public');
+  },
+  filename: (req, file, cb) => {
+    //extract properties to create file name
+    const { category, brand, gender, color } = req.body;
+
+    //extract original name and file extension
+    const { originalname } = file;
+    const fileExtension = originalname.split('.')[1];
+
+    //create part of the name out of the post fields
+    const filename = `${category}_${gender}-${brand}-${color}-${Date.now()}.${fileExtension}`;
+
+    cb(null, filename);
+  },
+});
+// console.log({ test: multerStorage.getFilename() });
+module.exports = multerStorage;
