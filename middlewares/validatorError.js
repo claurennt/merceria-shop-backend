d49c67107e18ba8447691177353b64082776ef89
claurennt <c.benedetti1989@gmail.com>
2021-09-28 12:27:12 +0200

    add param and error validator middleware

---
+++ middlewares/validatorError.js
@@ -0,0 +1,15 @@
+const { validationResult } = require('express-validator');
+
+const validatorError = (req, res, next) => {
+  const result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    // Response will contain something like
+    // { errors: [ "body[password]: must be at least 10 chars long" ] }
+    return res.json({ errors: result.array() });
+  }
+
+  next();
+};
+
+module.exports = validatorError;
