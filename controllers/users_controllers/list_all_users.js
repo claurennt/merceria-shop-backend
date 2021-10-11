601c5ca2d1877b31190703000397785cff27ee26
claurennt <c.benedetti1989@gmail.com>
2021-10-11 22:58:05 +0200

    add connection to multiple dbs, add create user fn, add jwb token, add auth admin(not finished)

---
+++ controllers/users_controllers/list_all_users.js
@@ -0,0 +1,11 @@
+const User = require('../../db/models/UsersModel');
+
+const list_all_users = async (req,res,next)=>{
+    try{
+        const users = await User.find({})
+        res.status(200).json(users)
+    }catch(err){
+        next(err)
+    }
+}
+module.exports = list_all_users;
